9c57c139070214b3fedc600a462542107e560b44
Rmaki27 <engineermaki27@gmail.com>
2023-06-01 20:21:11 -0700

    Fix spacing and remove scroll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,12 @@ import githubIcon from '../assets/Github-Icon.png'
 export default function Footer() {
     return (
         <div className="footer">
-            <img src={twitterIcon} alt="Twitter icon" />
-            <img src={fbIcon} alt="Facebook icon" />
-            <img src={instaIcon} alt="Instagram icon" />
-            <img src={githubIcon} alt="Github icon" />
-        
+            <div className='icons-container'>
+                <img src={twitterIcon} alt="Twitter icon" />
+                <img src={fbIcon} alt="Facebook icon" />
+                <img src={instaIcon} alt="Instagram icon" />
+                <img src={githubIcon} alt="Github icon" />
+            </div>
         </div>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@ export default function Interests() {
     return (
         <div>
             <h2 className="interests__section-title">Interests</h2>
-            <p className="paragraph">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="interests__paragraph">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
 
     )
