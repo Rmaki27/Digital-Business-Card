e00bda9a1700623c4ae3f7c22dd56bee94310e72
Rmaki27 <engineermaki27@gmail.com>
2023-06-01 20:31:38 -0700

    Clean up button styling

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,5 +1,6 @@
 import headshot from '../assets/headshot.jpg'
-
+import emailIcon from '../assets/Mail-Icon.png'
+import linkedInIcon from '../assets/LinkedIn-Icon.png'
 
 export default function Info() {
     return (
@@ -8,9 +9,15 @@ export default function Info() {
             <h1 className="name">Ryan Maki</h1>
             <h4 className="career-title">Frontend Developer</h4>
             <p className="website">makii.webdev.com</p>
-            <div>
-                <button className="email-btn">Email</button>
-                <button className="linkedin-btn">LinkedIn</button>
+            <div className='btn-container'>
+                <button className="email-btn">
+                    <img src={emailIcon} className='btn-icons' alt="mail icon" />
+                    Email
+                    
+                    </button>
+                <button className="linkedin-btn">
+                    <img src={linkedInIcon} className='btn-icons' alt="LinkedIn icon" />
+                    LinkedIn</button>
             </div>
         </div>
     )
